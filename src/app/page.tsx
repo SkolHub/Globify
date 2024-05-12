abc638e15b5f82b6c5bdf92f29bb3b806cead14d
Karo8870 <cazacuchristian@gmail.com>
2024-05-12 23:41:01 +0300

    Fixed flavia

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
 'use client';
 
 import '@/lib/fontawesome/css/fa.css';
-import { MapContextProvider } from '@/lib/logic/MapContext';
 import Map from '@/components/map';
 
 export default function Home() {
   return (
     <>
       {/*<Navbar />*/}
-      <MapContextProvider>
-        <main className='overflow-hidden'>
-          <Map />
-          {/*<SidePanel data={data} />*/}
-          {/*<LayersDropdown layers={layers} setLayers={setLayers} />*/}
-        </main>
-      </MapContextProvider>
+      <main className='overflow-hidden'>
+        <Map />
+        {/*<SidePanel data={data} />*/}
+        {/*<LayersDropdown layers={layers} setLayers={setLayers} />*/}
+      </main>
     </>
   );
 }
