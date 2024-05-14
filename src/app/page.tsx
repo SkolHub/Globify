99ff9bfb8ea6385ebb8d087486d5f7d564953fbe
Karo8870 <cazacuchristian@gmail.com>
2024-05-15 00:18:19 +0300

    Made climate zones work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,18 @@
 
 import '@/lib/fontawesome/css/fa.css';
 import dynamic from 'next/dynamic';
+import { data } from '@/lib/data';
+import SidePanel from '@/components/side-panel';
 
-const LazyMap = dynamic(() => import('@/components/map'), { ssr: false });
+const Map = dynamic(() => import('@/components/map'), { ssr: false });
 
 export default function Home() {
   return (
     <>
       {/*<Navbar />*/}
       <main className='overflow-hidden'>
-        <LazyMap />
-        {/*<SidePanel data={data} />*/}
+        <Map />
+        <SidePanel data={data} />
         {/*<LayersDropdown layers={layers} setLayers={setLayers} />*/}
       </main>
     </>
--- src/components/map.tsx
+++ src/components/map.tsx
@@ -2,14 +2,48 @@
 
 import Globe from 'react-globe.gl';
 import useResizeHook from '@/lib/hooks/useResizeHook';
+import data from '@/lib/data/climate-zones.json';
+import { useState } from 'react';
 
 function Map() {
   const [width, height] = useResizeHook();
 
+  const [hoveredP, setHoveredP] = useState<object | null>(null);
+
+  let i = 0,
+    j = 0;
+
   return (
     <Globe
-      globeImageUrl='/02c32a7f118f69842a3bfa3a02bd5b12.webp'
+      globeImageUrl='/earth-blue-marble.jpg'
       backgroundImageUrl='/night-sky.png'
+      polygonAltitude={(pol) => (pol === hoveredP ? 0.05 : 0.01)}
+      // polygonCapColor={(pol) => {
+      //   i++;
+      //   if (colors[i] && pol !== hoveredP) {
+      //     return colors[i] + '50';
+      //   }
+      //
+      //   return colors[i];
+      // }}
+      // polygonSideColor={() => {
+      //   j++;
+      //
+      //   if (colors[j]) {
+      //     return colors[j] + '20';
+      //   }
+      //
+      //   return colors[j];
+      // }}
+      onPolygonHover={(pol) => {
+        setHoveredP(pol);
+      }}
+      polygonSideColor={() => '#00000000'}
+      polygonCapColor={(pol) => (pol === data[0] as any ? '#F0000020': '#0000F020')}
+      polygonStrokeColor={(pol) => (pol === data[0] as any ? '#F0000020': '#0000F020')}
+      polygonsData={data as any}
+      polygonsTransitionDuration={100}
+      // polygonLabel={(pol) => (pol as any).properties.admin}
       width={width}
       height={height}
     />
