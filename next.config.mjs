a1030d4a1c8057f26106081f0b2cacef26bbb584
alex <simedrea.alex@gmail.com>
2025-01-06 11:45:45 +0200

    added redirect to english when on /

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,15 @@ const nextConfig = {
   reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true
+      }
+    ];
   }
 };
 
